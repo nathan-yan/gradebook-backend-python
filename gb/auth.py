d99e5a86a26b1a3a8687f6e178fc37f98fc43d04
Nathan Yan <s-yann@bsd405.org>
2018-12-27 19:06:49 -0800

    Replaced secrets module (only in python 3.6 apparently) with os.urandom

--- gb/auth.py
+++ gb/auth.py
@@ -1,5 +1,16 @@
-import secrets
+import os
 
+# If we're using Python 3.6 or above we should use secrets
 def generate_token(length = 16):
-    return secrets.token_urlsafe(length)
+    random_bytes = os.urandom(length)
+
+    valid_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-'
+
+    res = ""
+    for byte in random_bytes:
+        res += valid_characters[ord(byte) % len(valid_characters)]
+
+    return res
+
+    # return secrets.token_urlsafe(length)
 
